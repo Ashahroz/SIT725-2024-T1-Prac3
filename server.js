bd9c90bf0f6ae8fc761cd6d628f3afb6bad56d8a
Ashahroz <127468075+Ashahroz@users.noreply.github.com>
2024-03-25 21:09:41 +1100

    Pract3

--- server.js
+++ server.js
@@ -1,30 +1,12 @@
-var express = require('express');
-var app = express();
-app.use(express.json());
+var express = require("express")
+const req =require("express/lib/request");
+var app = express()
+var port = process.env.port || 3000;
+
 app.use(express.urlencoded({extended:false}));
-var port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.static(__dirname +'/public'));
 
-
-app.use(express.static(__dirname + '/public'));
-app.listen(port, () => {
-    console.log('listening on port ' + port)
+app.listen(port,()=>{
+    console.log('App listening to:' +port)
 });
-
-app.get('/',function(req,res) {
-    res.render('index.html');
-});
-
-
-function complexMath(num1, num2){
-    return parseInt(num1)+parseInt(num2);
-};
-
-
-app.get('/addTwoNumbers', (req, res) =>{
-   var num1 = req.query.num1;
-   var num2 = req.query.num2;
-   var result = complexMath(num1,num2);
-   res.json({status: 200, data: result ,message: 'Success'});
-
-});
-
